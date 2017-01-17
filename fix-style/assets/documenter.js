3dbc61186c86f5f13ed22e89909df936d9a0b644
Morten Piibeleht <morten.piibeleht@gmail.com>
2017-01-17 15:30:22 +0200

    Update.

--- fix-style/assets/documenter.js
+++ fix-style/assets/documenter.js
@@ -52,14 +52,33 @@ require(['mathjax'], function(MathJax) {
 
 require(['jquery', 'highlight', 'highlight-julia'], function($, hljs) {
     $(document).ready(function() {
+        hljs.initHighlighting();
+    })
+
+})
+
+// update the version selector with info from the ../versions.js file
+require(['jquery'], function($) {
+    $(document).ready(function() {
         if (typeof DOC_VERSIONS !== 'undefined') {
             var version_selector = $("#version-selector");
+            var existing_versions = $('#version-selector > option');
+            var existing_versions_texts = existing_versions.map(function(i,x){return x.text});
             DOC_VERSIONS.forEach(function(each) {
-                var option = $("<option value='" + documenterBaseURL + "/../" + each + "'>" + each + "</option>");
-                version_selector.append(option);
+                var version_url = documenterBaseURL + "/../" + each;
+                var existing_id = $.inArray(each, existing_versions_texts);
+                // if not already in the version selector, add it as a new option,
+                // otherwise update the old option with the URL and enable it
+                if (existing_id == -1) {
+                    var option = $("<option value='" + version_url + "'>" + each + "</option>");
+                    version_selector.append(option);
+                } else {
+                    var option = existing_versions[existing_id];
+                    option.value = version_url;
+                    option.disabled = false;
+                }
             });
         }
-        hljs.initHighlighting();
     })
 
 })
