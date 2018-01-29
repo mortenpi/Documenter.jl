88f00cfeac9eb6f2df2184cc95bf9455b9b36b94
Morten Piibeleht <morten.piibeleht@gmail.com>
2018-01-29 21:45:02 +1300

    Fix coverage.jl

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,12 +1,8 @@
 # Only run coverage from linux nightly build on travis.
-get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.6" || exit()
+get(ENV, "TRAVIS_OS_NAME", "")       == "linux" || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.6"   || exit()
+cd(Pkg.dir("Documenter"))
 
 Pkg.add("Coverage")
 using Coverage
-
-println("pwd: $(pwd())")
-cd(joinpath(dirname(@__FILE__), "..")) do
-    println("pwd: $(pwd())")
-    Codecov.submit(Codecov.process_folder())
-end
+Codecov.submit(Codecov.process_folder())
